779a2cff17c68c0df2a0e93e0fb69ea6631ed576
“Robert <digitalmediaflow@gmail.com>
2020-11-12 15:57:53 -0500

    not finish quad

---
+++ quadratic.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// ax2 + bx + c 
+// a b c real numbers
+//a != 0
+
+int main(){
+    int a, b, c , quad; 
+    printf(" Please add input for a");
+    scanf("%d", &a);
+    printf(" Please add input for b");
+    scanf("%d", &b);
+    printf(" Please add input for c");
+    scanf("%d", &c);
+if(a != 0){
+    quad = a * a + 
+
+}
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
