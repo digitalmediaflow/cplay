4c23fb88d5d3a1d502809c2191071a3b2566553b
“Robert <digitalmediaflow@gmail.com>
2020-11-11 23:15:36 -0500

    natural numbers

---
+++ loop1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main(){
+/*
+1. Write a program in C to display the first 10 natural numbers. 
+Go to the editor
+Expected Output :
+1 2 3 4 5 6 7 8 9 10
+*/
+int numbers[10]={1,2,3,4,5,6,7,8,9,10}; 
+int i;
+for(i = 0; i <= numbers ; i++){
+    printf("%d")
+}
+
+
+
+
+// printf("numbers %c", numbers)
+
+
+
+}
---
+++ realNumbers.c
@@ -0,0 +1,18 @@
+
+// Write a program in C to display the first 10 natural numbers. 
+// Go to the editor
+// Expected Output :
+// 1 2 3 4 5 6 7 8 9 10
+// int numbers[10]={1,2,3,4,5,6,7,8,9,10}; // Just Reference if using array
+
+
+#include <stdio.h>
+ int main(){
+int i;
+for(i = 1; i <= 10 ; ++i){
+    printf("%d ", i);
+}
+    return 0;
+}
+
+
