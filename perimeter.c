b22ef8898f46b03db8d57146d158958fb9df4933
“Robert <digitalmediaflow@gmail.com>
2020-11-11 23:31:56 -0500

    added perimeter

---
+++ perimeter.c
@@ -0,0 +1,17 @@
+// Write a program in C that prints the perimeter of a rectangle to take its height and width
+// perimeter = 2 * (width + height)
+
+#include <stdio.h>
+ int main(){
+int width height p;
+printf("Please add the width");
+scanf("%d", &width);
+printf("Please add the height");
+scanf("%d", &width);
+p = 2 * (width + 2)
+printf("The permiter is $d", p);
+
+    return 0;
+}
+
+
